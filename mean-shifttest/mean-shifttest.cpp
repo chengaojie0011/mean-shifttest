29e1b3ea01545b40329971ff87049ea86624ac03
chengaojie0011 <598641996@qq.com>
2017-04-24 22:44:01 +0800

    通过直方图获取漫水天冲后的灰度值，获取天空所在区域的灰度值，并进行标记

--- mean-shifttest/mean-shifttest.cpp
+++ mean-shifttest/mean-shifttest.cpp
@@ -9,10 +9,29 @@
 #include<opencv2/highgui/highgui.hpp>  
 #include <opencv2/imgproc/imgproc.hpp>  
 #include <math.h>  
+#include <numeric>
+#include <algorithm>
+#include<vector>
+#include <set>
+
 using namespace cv;
 using namespace std;
+
 #define MIN_AREA 1000//定义最小有效的矩形面积
+
 vector<Vec2f> lines;//定义一个矢量结构lines用于存放得到的线段矢量集合  
+
+
+typedef  struct boxshore//声明一个结构体类型boxShore
+{
+	int num;
+	int x;  
+	int y;
+	int gray;
+}boxShoreInfo;
+
+
+typedef vector<boxShoreInfo> boxShoreInfoVec;
 
 //去高光处理
 void  rmHighlight(const Mat  &src, Mat &dst)
@@ -242,10 +261,10 @@ void   histogramMy(const Mat  &src, Mat &hisimage,Point  pout1,Point  pout2,int 
 									//首先得定义一个变量用来存储 单个维度的 数值的取值范围  
 	float midRanges[] = { 0, 256 };
 	const float *ranges[] = { midRanges };
-	cout <<"y="<< src.rows <<"x="<< src.cols << endl;
-	cout << "y2=" <<hisimage.rows << "x2=" << hisimage.cols << endl;
-	cout << "p1=" << pout1 << "p2=" << pout2 << endl;
-	cout << "p1.x=" << pout1.x << "p1.y=" << pout1.y << endl;
+	//cout <<"y="<< src.rows <<"x="<< src.cols << endl;
+	//cout << "y2=" <<hisimage.rows << "x2=" << hisimage.cols << endl;
+	//cout << "p1=" << pout1 << "p2=" << pout2 << endl;
+	//cout << "p1.x=" << pout1.x << "p1.y=" << pout1.y << endl;
 
 
 
@@ -384,6 +403,82 @@ void   histogramMy(const Mat  &src, Mat &hisimage,Point  pout1,Point  pout2,int 
 
 }
 
+void   histogramSky(const Mat  &src, Mat &hisimage, Point  pout1, Point  pout2, set<int> &hisvalue)
+{
+	Mat colorimage, grayimage;
+	//cvtColor(src, grayimage, CV_BGR2GRAY);
+	cvtColor(src, colorimage, CV_GRAY2BGR);//变为灰度图
+	cvtColor(colorimage, hisimage, CV_BGR2GRAY);
+
+	//为计算直方图配置变量  
+	//首先是需要计算的图像的通道，就是需要计算图像的哪个通道（bgr空间需要确定计算 b或g货r空间）  
+	int channels = 0;
+	//然后是配置输出的结果存储的 空间 ，用MatND类型来存储结果  
+	MatND dstHist;
+	//接下来是直方图的每一个维度的 柱条的数目（就是将数值分组，共有多少组）  
+	int histSize[] = { 256 };       //如果这里写成int histSize = 256;   那么下面调用计算直方图的函数的时候，该变量需要写 &histSize  
+									//最后是确定每个维度的取值范围，就是横坐标的总数  
+									//首先得定义一个变量用来存储 单个维度的 数值的取值范围  
+	float midRanges[] = { 0, 256 };
+	const float *ranges[] = { midRanges };
+
+	calcHist(&hisimage, 1, &channels, Mat(), dstHist, 1, histSize, ranges, true, false);
+
+	//calcHist  函数调用结束后，dstHist变量中将储存了 直方图的信息  用dstHist的模版函数 at<Type>(i)得到第i个柱条的值  
+	//at<Type>(i, j)得到第i个并且第j个柱条的值  
+
+	//开始直观的显示直方图——绘制直方图  
+	//首先先创建一个黑底的图像，为了可以显示彩色，所以该绘制图像是一个8位的3通道图像  
+	Mat drawImage = Mat::zeros(Size(256, 256), CV_8UC3);
+	//因为任何一个图像的某个像素的总个数，都有可能会有很多，会超出所定义的图像的尺寸，针对这种情况，先对个数进行范围的限制  
+	//先用 minMaxLoc函数来得到计算直方图后的像素的最大个数  
+	double g_dHistMaxValue;
+	minMaxLoc(dstHist, 0, &g_dHistMaxValue, 0, 0);
+	//将像素的个数整合到 图像的最大范围内  
+	//遍历直方图得到的数据  
+
+	for (int i = 0; i < 256; i++)
+	{
+		int value = cvRound(dstHist.at<float>(i) * 256 * 0.9 / g_dHistMaxValue);
+		/*if (dstHist.at<float>(i)!=0 && dstHist.at<float>(i)!=255&&value>=50)
+		{*/
+		if (i!=0 && i!=255 && dstHist.at<float>(i)>500)
+		{
+			hisvalue.insert(i);
+			//cout <<"灰度值="<<i << " 像素量="<< dstHist.at<float>(i) << endl;
+		}
+		//cout << dstHist.at<float>(i) << endl;
+		line(drawImage, Point(i, drawImage.rows - 1), Point(i, drawImage.rows - 1 - value), Scalar(255, 0, 0));
+	}
+	int  minValue = 1;
+	int gmin, gmax;
+	for (int i = 0; i < 256; i++)
+	{
+		float iminValue = dstHist.at<float>(i);
+		if ((iminValue > minValue) && (i > 2))
+		{
+			gmin = i;
+			break;
+		}
+	}
+	//  寻找直方图的右端  
+	int imax = 255;
+	for (int i = 255; i >= 0; i--)
+	{
+		float imaxValue = dstHist.at<float>(i);
+		if ((imaxValue > minValue) && (i < 254))
+		{
+			gmax = i;
+			break;
+		}
+	}
+	//cout << "gmax=" << gmax<< "gmin=" << gmin << endl;
+	imshow("【直方图】", drawImage);
+
+
+}
+
+
 void separateShore(const Mat &src, Mat &dst, Point  pout1, Point  pout2)
 {
 	Mat res; //分割后图像  
@@ -396,9 +491,13 @@ void separateShore(const Mat &src, Mat &dst, Point  pout1, Point  pout2)
 	Mat out111;
 	int a1 = 0;
 
+
+
 	cvtColor(src, grayImage, CV_BGR2GRAY);//变为灰度图
 
 	rmHighlight(grayImage, out1);							  //高亮处理
+
+
 
 	cvtColor(out1, out11, CV_GRAY2BGR);//变为彩色图
 									   //imshow("caise", out11);
@@ -415,30 +514,128 @@ void separateShore(const Mat &src, Mat &dst, Point  pout1, Point  pout2)
 	int spatialRad = 30;  //空间窗口大小  
 	int colorRad = 30;   //色彩窗口大小  
 	int maxPyrLevel = 2;  //金字塔层数  
-	pyrMeanShiftFiltering(out2, dst, spatialRad, colorRad, maxPyrLevel); //色彩聚类平滑滤波  
-	imshow("test1", dst);
-
+	Mat dst1;
+	pyrMeanShiftFiltering(out2, dst1, spatialRad, colorRad, maxPyrLevel); //色彩聚类平滑滤波  
+	imshow("test1", dst1);
+	dst1.copyTo(dst);
 	RNG  rng = theRNG();
 	Mat mask(src.rows + 2, src.cols + 2, CV_8UC1, Scalar::all(0));  //掩模  
 	for (int y = 0; y < src.rows; y++)
 	{
 		for (int x = 0; x < src.cols; x++)
 		{
-			if (mask.at<uchar>(y + 1, x + 1) == 0)  //非0处即为1，表示已经经过填充，不再处理  
+			if (y <= (x*(pout1.y - pout2.y) / (pout1.x - pout2.x) + pout2.y))
 			{
-				Scalar newVal(rng(256), rng(256), rng(256));
-				floodFill(dst, mask, Point(x, y), newVal, 0, Scalar::all(5), Scalar::all(5)); //执行漫水填充  
+				if (mask.at<uchar>(y + 1, x + 1) == 0)  //非0处即为1，表示已经经过填充，不再处理  
+				{
+
+					Scalar newVal(rng(256), rng(256), rng(256));
+					floodFill(dst1, mask, Point(x, y), newVal, 0, Scalar::all(5), Scalar::all(5)); //执行漫水填充  
+
+				}
+			}
+		}
+	}
+	imshow("test11", dst1);
+	Mat grayImage2;
+	cvtColor(dst1, grayImage2, CV_BGR2GRAY);//变为灰度图
+
+	//imshow("test11", dst);
+	for (int y = 0; y < src.rows; y++)
+	{
+		for (int x = 0; x < src.cols; x++)
+		{
+
+			if (y > (x*(pout1.y - pout2.y) / (pout1.x - pout2.x) + pout2.y))
+			{
+			grayImage2.at<uchar>(y, x) = 0;
 
 			}
 		}
 	}
 
-	
-	Mat grayImage2;
-	cvtColor(dst, grayImage2, CV_BGR2GRAY);//变为灰度图
-	//imshow("test2", dst);
-	imshow("test3", grayImage2);
+	set<int> hisvalue;
+	Mat  histogramimage;
+	histogramSky(grayImage2, histogramimage, pout1, pout2, hisvalue);
+		//cout << "set 的 size 值为 :" << hisvalue.size() << endl;
+		set<int>::iterator it; //定义前向迭代器 
+							   //中序遍历集合中的所有元素  
+		int pixmin = 0,pixflood=0;
+	//imshow("test1111",grayImage2);
+		for (it = hisvalue.begin(); it != hisvalue.end(); it++)
+		{
+			
+				int pixnumber = 0;
+				long pixvaluenum = 0;
+				int pixaver = 0;
+				//cout << "set 的 值为 :" << *it << endl;
+			for (int y = 0; y < src.rows; y++)
+			{
+				for (int x = 0; x < src.cols; x++)
+				{
 
+					if (y < (x*(pout1.y - pout2.y) / (pout1.x - pout2.x) + pout2.y))
+					{
+						//cout << "set 的 值为 :" << *it<< endl;
+						if (grayImage2.at<uchar>(y, x) == *it)
+						{
+							pixnumber++;
+							pixvaluenum = pixvaluenum + dst.at<uchar>(y, x);
+					
+						}
+					}
+				}
+			}
+			
+			pixaver = pixvaluenum / pixnumber;
+				if (pixmin <= pixaver)
+			{
+			pixmin = pixaver;
+			pixflood = *it;
+			}
+		
+
+		}
+		//cout << " pixmin  的 值为 :" << pixmin << endl;
+		//cout << " pixflood 的 值为 :" << pixflood << endl;
+		Point skycenter;
+		int skyleft=0,skyright=0,skytop=0,skybottom=0;
+		for (int y = 0; y < src.rows; y++)
+		{
+			for (int x = 0; x < src.cols; x++)
+			{
+			
+				if (y < (x*(pout1.y - pout2.y) / (pout1.x - pout2.x) + pout2.y))
+				{
+					if (grayImage2.at<uchar>(y, x) == pixflood)
+					{
+						if (x<=skyleft)
+						{
+							skyleft = x;
+						}
+						if (x >= skyright)
+						{
+							skyright= x;
+						}
+						if (y<= skytop)
+						{
+							skytop= y;
+						}
+						if (y >= skybottom)
+						{
+							skybottom = y;
+						}
+					}
+
+				}
+			}
+		}
+
+		
+		skycenter.x = (skyleft + skyright) / 2;
+		skycenter.y = (skytop + skybottom) / 2;
+		putText(dst1, "sky", skycenter, CV_FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 2, 2); 
+		imshow("test1111", dst1);
 }
 
 void  boatDetection(const Mat  &src, Mat &dst,Point  pout1, Point  pout2,int gmax,int gmin)
@@ -547,8 +744,8 @@ int main(int argc, char** argv)
 	histogramMy(outMeanshift,histogram,pout1,pout2,hismax,hismin);
   //  imshow("his", histogram);
 	int x = histogram.at<uchar>(178, 1);
-cout << "gmax=" << hismax << "gmin=" << hismin << endl;
-cout << "像素值=" <<x<< endl;
+//cout << "gmax=" << hismax << "gmin=" << hismin << endl;
+//cout << "像素值=" <<x<< endl;
 	boatDetection(histogram, test1,pout1, pout2, hismax, hismin);
 	//imshow("test1", test1);
 
